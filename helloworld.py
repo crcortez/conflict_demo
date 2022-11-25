ba70d0c9234f9fa6dc23830c1dd0c93c7d6efb57
Chanella Cortez <chanellacortez@gmail.com>
2022-11-24 17:38:56 -0800

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> eb2b6019aa58b3e5ac46f50754d7b866ee26a184
 
 
 main()
